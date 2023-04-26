412b0f276ee84678f0ed19fd5cf749da945910be
tik-ta <75075406+tik-ta@users.noreply.github.com>
2023-04-26 19:35:55 +0300

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,26 @@
+class Settings:
+    chrome_driver = "/tests/chromedriver.exe"
+
+    base_url = 'https://b2c.passport.rt.ru'
+    valid_email = 'm2aystj15@mozmail.com'       # Ввести валидный email
+    valid_password = 'rn8,WQhiLC69Cw8'   # Ввести валидный пароль
+    valid_email_reg = 'Ввести валидный email'   # Ввести валидный email
+    invalid_email = 'ne_sparsit.bazu.com'
+    invalid_password = 'sparsit_740'
+    empty_email = ''
+    empty_password = ''
+    menu_type_auth = ['Телефон', 'Почта', 'Логин', 'Лицевой счёт']
+    placeholder_name = ['Мобильный телефон', 'Электронная почта', 'Логин', 'Лицевой счёт']
+    first_name = 'Эдуард'
+    last_name = 'Карлович'
+    first_name_ = '-'
+    last_name_ = '-'
+    russian_generate_string = 'я'
+    latin_generate_string = 'invalid'
+    chinese_chars = ' 青山河凤龙月星雨'
+    special_chars = '()_+=/{}!@#$%^&*'
+    numbers = 740
+    empty = ''
+    passw1 = '12345678'
+    passw2 = 'qwerty'
+    passw3 = 'y5VxUwNzJb8KtLpD'
